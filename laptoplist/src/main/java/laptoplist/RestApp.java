2ba4d1487ce3f56019b6e1e30eb12ef44a80aae5
kfbb <iyezi@hotmail.com>
2017-02-26 19:09:53 +0000

    map rest apis to '/api/'

--- laptoplist/src/main/java/laptoplist/RestApp.java
+++ laptoplist/src/main/java/laptoplist/RestApp.java
@@ -3,7 +3,8 @@ package laptoplist;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+// if mapped to '/', all static files won't be served by JBoss
+@ApplicationPath("/api")
 public class RestApp extends Application{
 
 }
